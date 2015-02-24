61bcac3124f39124256037b0b075c6ca39e1c915
Aaron Xu <yiranxu.usu@gmail.com>
2015-02-24 14:40:26 -0700

    updated for inlegal items

--- file_read.py
+++ file_read.py
@@ -1,6 +1,7 @@
 ## read a file from command
 ## count the number of float numbers
 ## compute the sum of numbers
+## if there exists non-number, omit it, only count numbers
 
 import io
 
@@ -13,12 +14,21 @@ content=file.readlines()
 
 numsum=0.0
 numcount=0
+def isNumber(number):
+    try:
+        float(number)
+        return True
+    except ValueError:
+        return False
+        
 for line in content:
     for item in line.split():
-        numsum+=float(item)
-        numcount+=1
-        
-
+        item_is_number = isNumber(item)
+        while item_is_number:
+            numsum+=float(item)
+            numcount+=1
+            item_is_number=False
+            
 print numsum
 print numcount      
 
